1089744229e0e7bdb28180f911a6df8458f92cec
akmalropip <akmalropip@gmail.com>
2026-05-18 16:07:27 +0700

    membuat class rekeningbank

--- tugasPraktikum.cpp
+++ tugasPraktikum.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Membuat Abstract Base Class
+class RekeningBank
+{
+protected:
+    string namaNasabah;
+    double saldo;
+
+public:
+    RekeningBank(string nama, double s)
+    {
+        namaNasabah = nama;
+        saldo = s;
+    }
+
+    virtual void potongAdmin() = 0;
+
+    void tampilkanSaldo()
+    {
+        cout << "Nama Nasabah : " << namaNasabah << endl;
+        cout << "Saldo Akhir  : Rp " << saldo << endl;
+    }
+
+    virtual ~RekeningBank() {}
+};
